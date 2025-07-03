d68fecc2086ed753f14fcba0a085cff92e72dcf6
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-03 20:41:05 -0300

    fix slug creation logic

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -12,20 +12,26 @@ export class Answer extends Entity<AnswerProps> {
   static create(props: Optional<AnswerProps, "createdAt">, id?: UniqueEntityId): Answer {
     return new Answer({ ...props, createdAt: new Date() }, id)
   }
-  
-  get content() {
-    return this.props.content
-  }
-  
+
   get authorId() {
     return this.props.authorId
   }
-  
+
   get questionId() {
     return this.props.questionId
   }
 
+  get content() {
+    return this.props.content
+  }
+  set content(content: string) {
+    this.update()
+    this.props.content = content
+  }
   get excerpt() {
     return this.content.substring(0, 120).trimEnd().concat("...")
+  }
+  private update() {
+    this.props.updatedAt = new Date()
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -16,31 +16,59 @@ export class Question extends Entity<QuestionProps> {
   get title() {
     return this.props?.title
   }
-  
+
   get content() {
     return this.props?.content
   }
-  
+
+  set title(title: string) {
+    this.props.slug = Slug.createFromText(title)
+    this.props.title = title
+  }
+
+  set content(content: string) {
+    this.props.content = content
+  }
+
   get authorId() {
     return this.props?.authorId
   }
-  
+
   get bestAnswerId() {
     return this.props?.bestAnswerId
   }
-  
+  set bestAnswerId(bestAnswerId:UniqueEntityId) {
+    this.props.bestAnswerId = bestAnswerId
+    this.update()
+  }
+
   get slug() {
     return this.props?.slug
   }
   get createdAt() {
     return this.props?.createdAt
   }
-  
+
   get updatedAt() {
     return this.props?.updatedAt
   }
-  
-  static create(props: Optional<QuestionProps, "createdAt">, id?: UniqueEntityId): Question {
-    return new Question({ ...props, createdAt: new Date() }, id)
+
+  static create(
+    props: Optional<QuestionProps, "createdAt" | "slug">,
+    id?: UniqueEntityId
+  ): Question {
+    return new Question(
+      { ...props, slug: props.slug ?? Slug.createFromText(props.title), createdAt: new Date() },
+      id
+    )
+  }
+
+  private update() {
+    this.props.updatedAt = new Date()
+  }
+
+  get excerpt() {
+    this.update()
+    return this.content.substring(0, 120).trimEnd().concat("...")
   }
 }
