338eaf8bd7ae5931c6facd75d4f6c0a8bd4a0c39
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-04 12:25:26 -0300

    add question deletion test

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -13,8 +13,22 @@ describe("Delete question", () => {
   it("Should be able to Delete a question", async () => {
     const newQuestion = makeQuestion({})
     await questionsRepository.create(newQuestion)
-    await sut.execute({ questionId: newQuestion.id.toString() })
+    await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: newQuestion.authorId.toString(),
+    })
     const questionFound = await questionsRepository.findById(newQuestion.id.toString())
     expect(questionFound).toEqual(null)
+  })
+  it("Should not be  be able to Delete a question from another author", async () => {
+    const newQuestion = makeQuestion({})
+    await questionsRepository.create(newQuestion)
+
+    expect(async () => {
+      return await sut.execute({
+        questionId: newQuestion.id.toString(),
+        authorId: "test",
+      })
+    }).rejects.toThrowError()
   })
 })
--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -2,6 +2,7 @@ import { IQuestionRepository } from "../repositories/questions-repository"
 
 interface DeleteQuestionUseCaseRequest {
   questionId: string
+  authorId:string
 }
 
 interface DeleteQuestionUseCaseResponse {}
@@ -10,10 +11,14 @@ export class DeleteQuestionUseCase {
   constructor(private questionsRepository: IQuestionRepository) {}
   async execute({
     questionId,
+    authorId
   }: DeleteQuestionUseCaseRequest): Promise<DeleteQuestionUseCaseResponse> {
     const question = await this.questionsRepository.findById(questionId)
     if (!question) {
       throw new Error("Question not found")
+    }
+    if (question.authorId.toString() !== authorId) {
+      throw new Error("Unauthorized")
     }
     await this.questionsRepository.delete(question)
     return {}
