33f16226f8f14c67c95bd13c74bd09cb0fb8cd15
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-03 21:56:17 -0300

    fix linting installation

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,9 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const compat = new FlatCompat({
-    baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
-export default defineConfig([{
-    extends: compat.extends("@rocketseat/eslint-config/node"),
-}]);
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+]);
