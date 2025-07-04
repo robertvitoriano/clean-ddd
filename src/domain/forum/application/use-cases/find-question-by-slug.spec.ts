c0c867152a8c75a6434908e635481c6188f148cc
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-04 11:26:11 -0300

    testing question finding by slug

--- src/domain/forum/application/use-cases/find-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/find-question-by-slug.spec.ts
@@ -1,20 +1,28 @@
 import { InMemoryQuestionsRepository } from "@/test/repositories/in-memory-questions-repository"
 import { IQuestionRepository } from "../repositories/questions-repository"
 import { FindQuestionBySlugUseCase } from "./find-question-by-slug"
+import { Question } from "../../enterprise/entities/question"
+import { Slug } from "../../enterprise/entities/value-objects/slug"
+import { UniqueEntityId } from "@/core/entities/unique-entity-id"
 
 let questionsRepository: IQuestionRepository
 let sut: FindQuestionBySlugUseCase
 beforeEach(() => {
   questionsRepository = new InMemoryQuestionsRepository()
   sut = new FindQuestionBySlugUseCase(questionsRepository)
-  
 })
 describe("Create question", () => {
   it("Should be able to get a question by slug", async () => {
-    const slug = "questionInfo"
-    const result = await sut.execute({slug})
+    const slug = Slug.createFromText("Testing slug now")
+    const question = Question.create({
+      title: "Test title",
+      content: "come testing content",
+      slug,
+      authorId: new UniqueEntityId(),
+    })
+    await questionsRepository.create(question)
+    const result = await sut.execute({ slug: slug.value })
 
-    expect(result.question).toEqual(null)
-
+    expect(result.question?.slug.value).toEqual(slug.value)
   })
 })
