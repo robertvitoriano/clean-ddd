e565a8703b77550febf6773d5f2e721f8028a0bc
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-03 21:47:29 -0300

    configure eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const compat = new FlatCompat({
+    baseDirectory: path.dirname(fileURLToPath(import.meta.url)),
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
 
 export default defineConfig([{
     extends: compat.extends("@rocketseat/eslint-config/node"),
