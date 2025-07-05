e9171f0b92e13a8a18ea2ae234fa897dac5f454f
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-05 11:45:41 -0300

    test ordering

--- src/domain/forum/application/use-cases/list-recent-questions.spec.ts
+++ src/domain/forum/application/use-cases/list-recent-questions.spec.ts
@@ -1,7 +1,6 @@
 import { InMemoryQuestionsRepository } from "@/test/repositories/in-memory-questions-repository"
 import { IQuestionsRepository } from "../repositories/questions-repository"
 import { ListRecentQuestionsCase } from "./list-recent-questions"
-import { Slug } from "../../enterprise/entities/value-objects/slug"
 import { makeQuestion } from "@/test/factories/make-question"
 
 let questionsRepository: IQuestionsRepository
@@ -20,5 +19,30 @@ describe("Create question", () => {
     const resultPage2 = await sut.execute({ page: 2 })
     expect(resultPage1.questions.length).to.equal(10)
     expect(resultPage2.questions.length).to.equal(10)
+    expect(resultPage1.questions[0].id).to.not.equal(resultPage2.questions[0].id)
+
+  })
+  
+  
+    it("Should be ordered", async () => {
+    for (let i = 0; i < 100; i++) {
+      const question = makeQuestion({
+        createdAt: new Date(Date.now() + i)
+      })
+      await questionsRepository.create(question)
+    }
+    let isOrdered = true
+    const resultPage1 = await sut.execute({ page: 1 })
+    
+    resultPage1.questions.forEach((question, index)=>{
+      const currentQuestionTime = question.createdAt.getTime()
+      const beforeQuestionIndex = index > 0 ? index - 1 : 0
+      const beforeQuestionTime = resultPage1.questions[beforeQuestionIndex].createdAt.getTime()
+      if(index > 0 && currentQuestionTime  < beforeQuestionTime){
+        isOrdered = false
+      }
+    })
+    expect(isOrdered).toBe(true)
+
   })
 })
