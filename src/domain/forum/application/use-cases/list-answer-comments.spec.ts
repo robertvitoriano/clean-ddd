c14d40e9ab0cae9924048055d8a5eb5e76de7bc3
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-07-08 15:35:13 -0300

    test if pages exceed maximum avalable when querying answer comments

--- src/domain/forum/application/use-cases/list-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/list-answer-comments.spec.ts
@@ -20,21 +20,23 @@ describe("List answer comments", () => {
   it("Should be able to list comments by answer id", async () => {
     const answer = makeAnswer({})
     await answersRepository.create(answer)
-    
-    for(let i = 0;i < 30;i++){
-      await answerCommentRepository.create(makeAnswerComment({answerId:answer.id}))
+
+    for (let i = 0; i < 30; i++) {
+      await answerCommentRepository.create(makeAnswerComment({ answerId: answer.id }))
     }
     const resultPage1 = await sut.execute(answer.id.toValue(), 1)
     expect(resultPage1.comments.length).to.equal(10)
-
+    const resultPage2 = await sut.execute(answer.id.toValue(), 3)
+    expect(resultPage2.comments.length).to.equal(10)
+    const resultPage3 = await sut.execute(answer.id.toValue(), 4)
+    expect(resultPage3.comments.length).to.equal(0)
   })
-    it("Should throw an error if answer do not exist", async () => {
+  it("Should throw an error if answer do not exist", async () => {
     const answer = makeAnswer({})
-    
-    for(let i = 0;i < 30;i++){
-      await answerCommentRepository.create(makeAnswerComment({answerId:answer.id}))
+
+    for (let i = 0; i < 30; i++) {
+      await answerCommentRepository.create(makeAnswerComment({ answerId: answer.id }))
     }
     await expect(sut.execute(answer.id.toValue(), 1)).rejects.toThrowError()
-
   })
 })
